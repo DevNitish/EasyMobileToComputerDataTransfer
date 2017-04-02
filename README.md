e18af572d152e9af3d3127714abb048a857bc21f
Nitish Kumar <nitishonspeed@gmail>
2017-04-02 19:45:19 +0530

    removed unused files

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
-# MEANseedApp
-Seed app for mean stack development. The Seed app contains basic code start building your node application.
+# EasyMobileToComputerDataTransfer
+Provides a web interface to transfer data from Mobile phone to computer.
 [Demo Here](http://mean-seedapp.herokuapp.com/)
 ### Prerequisites 
 Make sure you have installed following apps
 * NodeJs version 6.X.X
-* Mongo DB version 3.2.X
 
 ###To run the app
 
-* Clone the repository  `git clone https://github.com/DevNitish/MEANseedApp.git `
-* Change the name of the repository if you want otherwise `cd MEANseedApp`
+* Clone the repository  `git clone https://github.com/DevNitish/EasyMobileToComputerDataTransfer.git `
+* Change the name of the repository if you want otherwise `cd EasyMobileToComputerDataTransfer`
 * Install npm packages  ` npm install`
 * Start the Node App  `npm start`
 * Open th browser on port 4050 `http://localhost:4050/`
--- app.js
+++ app.js
@@ -3,19 +3,36 @@ var express =require('express');
 var app =express();
 var path =require('path');
 var favicon = require('serve-favicon');
-router =express.Router();
+var router =express.Router();
 var bodyParser=require('body-parser');
 var multer  = require('multer');
 var Upload = multer({ dest: './uploads/'});
-var mainRoute = require('./server/routes/mainRoute.js'); //for routing
+
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+      console.log("the body==",file)
+      var filename = file.originalname;
+        cb(null, filename);
+  }
+})
+//can be used to store files onserver side 
+var Upload =multer({ storage:storage });
+router.post('/testPost', Upload.single('file'), function(req, res,next) {
+       console.log(req.body) // this contains all text data
+    console.log("my fileu",req.file) // this is always an empty array
+//
+});
 
 
 //Use the requires
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico'))); //for website icon
 app.use(bodyParser.json());//Read and parse the data sent from HTML form
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use('/', mainRoute);// use this file for all your API calls
+app.use('/',router);
 //This is for view handle bar
 app.set('views', path.join(__dirname, 'public'));
 app.engine('html', require('ejs').renderFile);
